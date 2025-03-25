9da476e2b0fbf2fa5fcc014b53c712e93ec72e86
ashirwadexe <ashirwadbappy@gmail.com>
2025-03-26 00:13:54 +0530

    added daisey ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Setting from './pages/Setting'
+import Profile from './pages/Profile'
 
 const App = () => {
+
+  const appRouter = createBrowserRouter([
+    {
+      path:'/',
+      element: <Home/>
+    },
+    {
+      path:'/login',
+      element: <Login/>
+    },
+    {
+      path:'/signup',
+      element: <SignUp/>
+    },
+    {
+      path:'/setting',
+      element: <Setting/>
+    },
+    {
+      path:'/profile',
+      element: <Profile/>
+    },
+  ])
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={appRouter}/>
+    </div>
   )
 }
 
