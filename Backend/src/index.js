5c31f6f972230aa0fc15c8c705c57936b2899bf6
ashirwadexe <ashirwadbappy@gmail.com>
2025-03-26 15:32:18 +0530

    adding cors to index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,13 +4,19 @@ import { connectDB } from "../db/db.js";
 import userRouter from '../routes/user.route.js';
 import messageRouter from '../routes/message.route.js'
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser()); 
-
-const PORT = process.env.PORT || 8080;
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    })
+);
 
 //apis
 app.use('/api/v1/auth', userRouter);
